815c0657c69d585de0e867bbfde115a9ecb20d97
adrose <adon.rosen@gmail.com>
2024-12-18 11:09:02 -0500

    added slurm array submit script

---
+++ scripts/bash/submitBayesArray.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#SBATCH --mem=6G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --output=./outputText/r_output_%J_%a.txt
+#SBATCH --error=./errorText/r_error_%J_%a.txt
+#SBATCH --time=12:00:00
+#SBATCH --job-name=change_point_bayes
+#SBATCH --mail-user=adon.rosen@vanderbilt.edu
+#SBATCH --mail-type=ALL
+#SBATCH --chdir=/home/rosena/pinesRep
+#SBATCH --array=1-24
+
+module purge
+module load R/4.2.1
+
+echo ${SLURM_ARRAY_TASK_ID}
+echo "Submitting job"
+date
+start=`date +%s`
+Rscript ./scripts/rCode/sysCall.r ${SLURM_ARRAY_TASK_ID}
+echo "Done"
+end=`date +%s`
+runtime=$((end-start))
+date
+echo " Total runtime:" 
+echo ${runtime}
---
+++ scripts/rCode/sysCall.r
@@ -0,0 +1,71 @@
+## This script will be used to run all of the 
+## bayesian cp models for the pfactor-nonlinear mauscript
+
+## load library(s)
+library(doParallel)
+library(rstan)
+
+# Now run through all of these model options in a for parallel loop
+mod.dv <- c("cbcl_scr_syn_internal_r", "cbcl_scr_syn_external_r")
+mod.iv <- c("cbcl_scr_syn_thought_r", "cbcl_scr_syn_attention_r", "cbcl_scr_syn_internal_r")
+all.mods <- expand.grid(mod.dv, mod.iv)
+all.mods$Var1 <- as.character(all.mods$Var1)
+all.mods$Var2 <- as.character(all.mods$Var2)
+all.mods <- all.mods[-which(as.character(all.mods$Var1)==as.character(all.mods$Var2)),]
+all.mods <- rbind(all.mods, c("cbcl_scr_syn_internal_r", "cbcl_scr_syn_external_r"))
+index.val <- 1
+all.dat <- list()
+for(z in 1:nrow(all.mods)){
+  ## Now go through the wave value
+  for(i in 1:4){
+    ## create the data
+    if(i == 1){
+      in.dat <- read.csv("./data/ABCD_CBCL_BL.csv")
+    }
+    if(i == 2){
+      in.dat <- read.csv("./data/ABCD_CBCL_Y1.csv")
+    }
+    if(i == 3){
+      in.dat <- read.csv("./data/ABCD_CBCL_Y2.csv")
+    }
+    if(i == 4){
+      in.dat <- read.csv("./data/ABCD_CBCL_Y3.csv")
+    }
+    if(i > 1){
+      in.dat.bp <- read.csv("./data/ABCD_CBCL_BL.csv")
+      in.dat.bp.family <- in.dat.bp[,c("src_subject_id", "site_id_l", "rel_family_id")]
+      in.dat <- merge(in.dat, in.dat.bp.family, by=c("src_subject_id", "site_id_l"), suffixes = c(".x",""))
+    }
+    tmp.dat <- in.dat
+    tmp.dat <- in.dat[complete.cases(tmp.dat[,c(all.mods$Var1[z], all.mods$Var2[z], "interview_age")]),]
+    data_jags <- list(
+      N = nrow(tmp.dat),
+      x = scale(tmp.dat[,all.mods$Var2[z]])[,],
+      y = tmp.dat[,all.mods$Var1[z]],
+      x2 = tmp.dat$interview_age,
+      N_group = as.numeric(factor(tmp.dat$site_id_l)),
+      count_group = length(unique(as.numeric(factor(tmp.dat$site_id_l)))),
+      #N_group2 = as.numeric(factor(tmp.dat$rel_family_id)),
+      #count_group2 = length(unique(as.numeric(factor(tmp.dat$rel_family_id)))),
+      beta = -5
+    )
+    all.dat[[index.val]] <- data_jags
+    index.val <- index.val + 1
+  }
+}
+i <- as.integer(commandArgs(1))
+stanmonitor <- c("a1","a2","b1","b2","b3","alpha","sigma_p")
+file.out <- paste("./data/brmsModsOut/model_InvLogitRandSite_", i, ".RDS", sep='')
+if(!file.exists(file.out)){
+  result_case = stan(file="./scripts/stan_models/updatedMultilevelInvLogit.stan", 
+                     data = all.dat[[i]], cores=1,chains=2,
+                     pars = stanmonitor, 
+                     iter=5000, warmup = 2000)
+  saveRDS(result_case, file.out)
+}else{
+  print("Done")
+}
+
+summary(do.call(rbind, 
+                args = get_sampler_params(result_case, inc_warmup = FALSE)),
+        digits = 2)
