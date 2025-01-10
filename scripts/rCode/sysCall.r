897a8c0eaca6c93e64b1962b119ade498f631739
adrose <adon.rosen@gmail.com>
2025-01-10 10:45:26 -0600

    Shift to scale X and Y into w/ integer counts

--- scripts/rCode/sysCall.r
+++ scripts/rCode/sysCall.r
@@ -1,9 +1,42 @@
 ## This script will be used to run all of the 
-## bayesian cp models for the pfactor-nonlinear mauscript
+## bayesian cp models for the pfactor-nonlinear manuscript
 
 ## load library(s)
 library(doParallel)
 library(rstan)
+
+## Decalre functions
+range01 <- function(x, minVal=NULL, maxVal=NULL, ...){
+  # Now make sure we have some standard deviation
+  if(is.na(sd(x, na.rm=T))){
+    output <- rep(1, length(x))
+    return(output)
+  }
+  if (sd(x, na.rm=T)==0 ){
+    output <- rep(1, length(x))
+    return(output)
+  }
+  # Now see if we are provided a max and min val
+  diffFlag <- 1
+  bothCheck <- 2
+  if(identical(NULL, minVal)){
+    minVal <- min(x, na.rm=T)
+    bothCheck <- bothCheck - 1
+    diffFlag <- 0
+  }
+  if(identical(NULL, maxVal)){
+    #print('foo')
+    maxVal <- max(x, na.rm=T)
+    bothCheck <- bothCheck - 1
+    diffFlag <- 0
+  }
+  if(diffFlag==0 & bothCheck!=0){
+    print(diffFlag)
+    print(bothCheck)
+    warning("Only one min or max value provided use caution!!")
+  }
+  (x-minVal)/diff(c(minVal, maxVal))
+}
 
 # Now run through all of these model options in a for parallel loop
 mod.dv <- c("cbcl_scr_syn_internal_r", "cbcl_scr_syn_external_r", "cbcl_scr_syn_attention_r", "cbcl_scr_syn_internal_r", "cbcl_scr_syn_thought_r", "cbcl_scr_syn_anxdep_r", "cbcl_scr_syn_withdep_r", "cbcl_scr_syn_somatic_r", "cbcl_scr_syn_aggressive_r", "cbcl_scr_syn_rulebreak_r")
@@ -13,7 +46,9 @@ all.mods <- expand.grid(mod.dv, mod.iv, iter)
 all.mods$Var1 <- as.character(all.mods$Var1)
 all.mods$Var2 <- as.character(all.mods$Var2)
 all.mods <- all.mods[-which(as.character(all.mods$Var1)==as.character(all.mods$Var2)),]
+#all.mods <- unique(all.mods)
 rowID <- as.integer(commandArgs(1))
+rowID <- 1
 i <- all.mods[rowID,3]
 ## Now go through the wave value
 ## create the data
@@ -38,8 +73,8 @@ tmp.dat <- in.dat
 tmp.dat <- in.dat[complete.cases(tmp.dat[,c(all.mods$Var1[rowID], all.mods$Var2[rowID], "interview_age")]),]
 data_jags <- list(
   N = nrow(tmp.dat),
-  x = tmp.dat[,all.mods$Var2[rowID]],
-  y = tmp.dat[,all.mods$Var1[rowID]],
+  x = round(scale(tmp.dat[,all.mods$Var2[rowID]])[,] + abs(min(scale(tmp.dat[,all.mods$Var2[rowID]])[,])),0),
+  y = round(scale(tmp.dat[,all.mods$Var1[rowID]])[,] + abs(min(scale(tmp.dat[,all.mods$Var1[rowID]])[,])),0),
   x2 = tmp.dat$interview_age,
   N_group = as.numeric(factor(tmp.dat$site_id_l)),
   count_group = length(unique(as.numeric(factor(tmp.dat$site_id_l)))),
@@ -47,14 +82,35 @@ data_jags <- list(
   count_group2 = length(unique(as.numeric(factor(tmp.dat$rel_family_id)))),
   beta = -5
 )
+# library(lme4)
+# for.glmer <- dplyr::bind_cols(data_jags)
+# mod.one <- glmer.nb(y ~ x + x2 + (1|N_group / N_group2), data = for.glmer)
+# qqnorm(residuals(mod.one))
+# qqline(residuals(mod.one),col=2)
+# mod.two <- glmer(y ~ x + x2 + (1|N_group2 / N_group), data = for.glmer, family="poisson")
+# qqnorm(residuals(mod.two))
+# qqline(residuals(mod.two),col=2)
+# mod.thr <- brm(
+#   y ~ x +
+#     x2 +
+#     (1|N_group2 / N_group),
+#   data = for.glmer,
+#   family = negbinomial(link = "log", link_shape = "log"),
+#   cores = 4)
+# denom.vec <- c(apply(in.dat[,126:136], 2, function(x) range(x, na.rm=TRUE)[2]))
+# lcm_vector <- function(x) Reduce(Lcm, x)
+# lcm_vector(c(20,50,75))
+# lcm_vector(denom.vec)
+
+## Now make the scaled values here
 all.dat <- data_jags
-stanmonitor <- c("a1","a2","b1","b2","b3","alpha","sigma_p", "sigma_p2", "rateVal")
-file.out <- paste("./data/brmsModsOut/model_InvLogitRandSite_allmods_", rowID, ".RDS", sep='')
+stanmonitor <- c("a1","a2","b1","b2","b3","alpha","sigma_p", "sigma_p2", "phi")
+file.out <- paste("./data/brmsModsOut/model_scaleXY_NB_allmods_", rowID, ".RDS", sep='')
 if(!file.exists(file.out)){
   result_case = stan(file="./scripts/stan_models/updatedMultilevelInvLogit.stan", 
                      data = all.dat, cores=2,chains=2,
                      pars = stanmonitor, 
-                     iter=25000, warmup = 5000, thin = 10, control = list(max_treedepth=11))
+                     iter=20000, warmup = 10000, thin = 5, control = list(max_treedepth=11))
   saveRDS(result_case, file.out)
 }else{
   print("Done")
